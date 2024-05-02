4b8e0a67750892b21c5846480c515c7f51d88674
davidsun202206 <davsun@suncor.com>
2024-05-02 09:51:16 -0400

    Add new table ddl

--- DDL/ddl_temp.sql
+++ DDL/ddl_temp.sql
@@ -2,5 +2,6 @@ create or replace table foo (
     col1 int,
     col2 varchar,
     col3 varchar,
-    col4 varchar
+    col4 varchar,
+    col5 varchar
 )
---
+++ DDL/ddl_temp2.sql
@@ -0,0 +1,7 @@
+create or replace table foo2 (
+    col1 int,
+    col2 varchar,
+    col3 varchar,
+    col4 varchar,
+    col5 varchar
+)
